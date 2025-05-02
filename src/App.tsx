8d783a4ab53433a735caeb779b8af011078d97ce
Shailesh Borkar <shailesh.borkar01@sap.com>
2025-05-02 11:02:16 +0530

    chore: remove unused useRef import to fix Vercel build lint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, ChangeEvent, useRef } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import './App.css';
 
 interface Comment {
